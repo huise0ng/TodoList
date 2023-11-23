4072ff2a0fe0eb94c053f63053ef2a28ef76acd9
huise0ng <shinhuiseong07@gmail.com>
2023-11-23 09:32:23 +0900

    ADD :: 데이터 저장 구현

---
+++ src/compositions/TodoListContainer.vue
@@ -0,0 +1,22 @@
+<template></template>
+
+<srcript>
+    import { ref, readonly, provide } from 'vue'
+    export default {
+        name: 'TodoListContainer',
+        setup() {
+            const todos = ref([])
+            provide('todos', readonly(tools))
+            const initTodos = (init_todos) => [
+                todos.value = init_todos
+            ]
+            const addTodo = (todo, date) => {}
+            const removeTodo = (id) => {}
+            const completeTodo= (id) => {}
+
+            provide('addTodo', addTodo)
+            provide('removeTodo', removeTodo)
+            provide('completeTodo', completeTodo)
+        },
+    }
+</srcript>
